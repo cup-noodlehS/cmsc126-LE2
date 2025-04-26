0c77b1fa0a29f74d598aa9ca418963180a7849ca
Panxi793 <oacananton@gmail.com>
2025-04-26 15:35:16 +0800

    Created Empty Budget Page and successful routing

---
+++ client/app/budget/page.tsx
@@ -0,0 +1,11 @@
+import { Layout } from "../components/layout/Layout";
+
+export default function BudgetPage() {
+  return (
+    <Layout>
+      <div className="p-6">
+        <h1 className="text-2xl font-bold text-gray-900 dark:text-white">Budget Page</h1>
+      </div>
+    </Layout>
+  );
+} 
