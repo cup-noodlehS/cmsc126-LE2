e78cff821e9de5bd9b5e304d760ca4d26f25f47d
cup-noodlehS <sheldonarthursagrado@gmail.com>
2025-04-29 09:26:15 +0800

    modify auth route

--- server/src/main/urls.py
+++ server/src/main/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/v1/account/", include("account.urls")),
+    path("api/v1/auth/", include("account.urls")),
     path("api/v1/budgethink/", include("budgethink.urls")),
 ]
